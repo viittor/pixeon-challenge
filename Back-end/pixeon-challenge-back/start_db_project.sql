d1791617797435b4e1924388b736b4f4e60a1e83
viittor <victor.ferrando@hotmail.coom>
2022-07-20 23:30:59 -0300

    added start db project file sql

--- Back-end/pixeon-challenge-back/start_db_project.sql
+++ Back-end/pixeon-challenge-back/start_db_project.sql
@@ -1,54 +1,29 @@
-create schema pixeonchallengeback
+create schema pixeonchallengeback;
 
-    create table healthcare
-    (
-        id   bigserial primary key,
-        name varchar(100),
-        cnpj varchar(14)
-    );
+create table healthcare (
+                            id bigserial primary key,
+                            name varchar(100),
+                            cnpj varchar(14),
+                            coins bigint);
 
-create table gender
-(
-    id   bigserial primary key,
-    name varchar(15)
-);
+create table exam (
+                      id bigserial primary key,
+                      id_healthcare bigint,
+                      pacient_name varchar(70),
+                      pacient_age bigint,
+                      pacient_gender varchar(20),
+                      physician_name varchar(70),
+                      physician_crm varchar(10),
+                      procedure_name varchar(255),
+                      recovered boolean,
+                      constraint fk_healthcare foreign key (id_healthcare) references healthcare(id));
 
-insert into gender(name)
-values ('masculino'),
-       ('feminino');
+insert into healthcare (name, cnpj, coins) values
+                                               ('Clinica Avenida', '05515946000142', 20),
+                                               ('Hospital Bom Jesus', '05515946000135', 20),
+                                               ('Centro Médico Health Plus', '05515946000154', 20);
 
-create table pacient
-(
-    id        bigserial primary key,
-    name      varchar(70),
-    age       bigint,
-    id_gender bigint,
-    constraint fk_gender foreign key (id_gender) references gender (id)
-);
-
-create table physician
-(
-    id   bigserial primary key,
-    name varchar(70),
-    crm  varchar(10)
-);
-
-create table procedure
-(
-    id   bigserial primary key,
-    name varchar(255)
-);
-
-create table exam
-(
-    id            bigserial primary key,
-    id_healthcare bigint,
-    id_pacient    bigint,
-    id_physician  bigint,
-    id_procedure  bigint,
-
-    constraint fk_healthcare foreign key (id_healthcare) references healthcare (id),
-    constraint fk_pacient foreign key (id_pacient) references pacient (id),
-    constraint fk_physician foreign key (id_physician) references physician (id),
-    constraint fk_procedure foreign key (id_procedure) references procedure (id)
-);
+insert into exam (id_healthcare, pacient_name, pacient_age, pacient_gender, physician_name, physician_crm, procedure_name) values
+                                                                                                                               (1, 'João da Silva', 15 , 'Masculino', 'Dr. Pedro Gouveia', '151256PR', 'Colonoscopia'),
+                                                                                                                               (2, 'Angela Oliveira', 45 , 'Feminino', 'Dr. Alberto Soares', '258655PR', 'Exame de Sangue'),
+                                                                                                                               (3, 'Marcelo Augusto', 180 , 'Masculino', 'Dr. Pedro Gouveia', '151256PR', 'Cirugia do Coração');
